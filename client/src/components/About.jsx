dfee502710a01fac71232fadf0dd77aaa81e4b55
Priyanshu Chaudhary <chaudharypriyanshu1@gmail.com>
2023-10-24 14:57:04 +0530

    second commit

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,9 +4,9 @@ const About = () => {
             <div className='mt-[10rem] ml-[5%] md:ml-[20%]' id="About">
                 <p className='text-white underline text-4xl font-medium decoration-solid underline-offset-[0.8rem]'>About</p>
                 <div className='mt-[7rem] text-white pr-[10%] text-lg pb-[7rem]'>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero odio voluptatum iste inventore! Reiciendis quia odit aspernatur at doloribus ea, voluptatum impedit. Error non impedit amet quasi vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, excepturi.
+                    <p>Hello! I'm Priyanshu Chaudhary, a final year B.Tech CSE student and a skilled full-stack web developer. I'm excited to bridge the gap between creativity and technology and turn ideas into stunning, functional websites.
                         <br /> <br />
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure fugiat neque commodi exercitationem molestias asperiores quia eum odio dolor voluptatibus fuga voluptatum iusto sequi necessitatibus mollitia blanditiis dolores accusamus consequuntur, optio labore. Ut explicabo repudiandae minus ratione consequatur numquam hic culpa officia, fugiat autem harum recusandae esse modi fuga sunt.
+                        With a background in computer science and a passion for web development, I specialize in crafting responsive, user-friendly websites. In my journey as a web developer, I've had the privilege of working on various projects that demonstrate my expertise. I'll work closely with you to understand your vision and goals, ensuring the final product aligns perfectly with your brand and objectives.
                     </p>
                 </div>
             </div>
